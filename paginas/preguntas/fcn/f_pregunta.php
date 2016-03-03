0a1ce6a59596ea7ec4e278ce1e84e24ea7857931
olopezdeveloper <oscarjoselopez26@gmail.com>
2016-03-03 08:18:24 +0100

    email

--- paginas/preguntas/fcn/f_pregunta.php
+++ paginas/preguntas/fcn/f_pregunta.php
@@ -24,6 +24,7 @@ switch($_POST["metodo"]){
 
 function guardarRespuesta()
 {
+	//die();
 	$publicacion = new publicaciones($_POST["pub_id"]);
 	$inse = $publicacion->setPreguntas($_POST["respuesta"],$_POST["id"]);	
 	$not = $publicacion->setNotificacion($_POST["pub_id"],$_POST["tipo"],$_POST["usr"],$inse);
@@ -39,19 +40,29 @@ function guardarPregunta()
 }	
 
 function sendPregunta(){
-		$cli = new usuario($_POST["usr_id"]);
-		$poster = new usuario($_POST["id_poster"]);
-		$publicacion = new publicaciones($_POST["pub_id"]);
-		ini_set("sendmail_from",$poster->a_email);
-		$email_to = "" . $cli-> a_email . "";
-
-		$email_subject = "Apreciodepana.com "." -  ".$poster-> n_nombre." ".$poster->n_apellido." tiene una pregunta!";
-		$email_message = "Sobre la publicacion:  ".$publicacion -> titulo." \n\n ".$_POST['pregunta'];	
+		
+	$destino = new usuario($_POST["usr_id"]);
+	$correo=new email();
 	
-		$headers = 'From: Apreciodepana.com ' . "\r\n" . 'Reply-To: '  . "no-reply@apreciodepana.com" . "\r\n" . 'X-Mailer: PHP/' . phpversion ();
-		mail ( $email_to, $email_subject, $email_message, $headers );
-		echo json_encode(array("correo a enviar"=>$cli-> a_email, "correo from"=>$poster->a_email,"header"=>$headers,"subject"=>$email_subject,"message"=>$email_message));			
-		//echo json_encode(array("estado"=>"OK"));
+	$link_detalle='http://vogueseshop.com/detalle.php?id='.$_POST["pub_id"];
+	$link_pregunta='http://vogueseshop.com/preguntas.php?tipo=1&publicacion='.$_POST["pub_id"];
+	$links=array("detalle"=>$link_detalle,"pregunta"=>$link_pregunta);
+	
+	$correo->sendPregunta($destino-> a_email , $links,  $_POST['pregunta']);	
+		/*
+	$cli = new usuario($_POST["usr_id"]);
+	$poster = new usuario($_POST["id_poster"]);
+	$publicacion = new publicaciones($_POST["pub_id"]);
+	ini_set("sendmail_from",$poster->a_email);
+	$email_to = "" . $cli-> a_email . "";
+	
+	$email_subject = "Apreciodepana.com "." -  ".$poster-> n_nombre." ".$poster->n_apellido." tiene una pregunta!";
+	$email_message = "Sobre la publicacion:  ".$publicacion -> titulo." \n\n ".$_POST['pregunta'];	
+	
+	$headers = 'From: Apreciodepana.com ' . "\r\n" . 'Reply-To: '  . "no-reply@apreciodepana.com" . "\r\n" . 'X-Mailer: PHP/' . phpversion ();
+	mail ( $email_to, $email_subject, $email_message, $headers );
+	echo json_encode(array("correo a enviar"=>$cli-> a_email, "correo from"=>$poster->a_email,"header"=>$headers,"subject"=>$email_subject,"message"=>$email_message));			
+	*///echo json_encode(array("estado"=>"OK"));
 }
 
 
@@ -64,7 +75,7 @@ function sendRespuesta(){
 	
 	$links=array("detalle"=>$link_detalle,"respuesta"=>$link_respuesta);
 	
-	$correo->sendRespuesta($destino-> a_email , $links);
+	$correo->sendRespuesta($destino-> a_email , $links,  $_POST['respuesta']);
 	/*
 		$cli = new usuario($_POST["usr_id"]);
 		$poster = new usuario($_POST["id_poster"]);
